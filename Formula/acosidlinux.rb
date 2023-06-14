dea18792da457d03dde90b146a8c9f2072accf9e
test <markus.punz@bdc.at>
2023-06-14 17:38:03 +0200

    mptest 0.0.1 (new formula)

--- Formula/acosidlinux.rb
+++ Formula/acosidlinux.rb
@@ -2,8 +2,8 @@ class Acosidlinux < Formula
   desc "Test MPTest"
   homepage "https://www.bdc.at"
   url "https://github.com/MarkusPunz/testapp1/raw/main/linux.tar.gz"
-  version "0.0.1"
-  sha256 "230c6bb6e03ec0c18a81ff1312ddbe1b356b6e8331a68903374a2c3164aa1428"
+  version "0.0.2"
+  sha256 "ee6da506919ab5eb75bf4e852035ac4a1c10c1533dddbe713a1dd8ae1515cfc8"
 
   on_linux do
     def install
