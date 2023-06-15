904668bd72a9d8a124f4860910b35b4ab57079d6
test <markus.punz@bdc.at>
2023-06-15 16:27:52 +0200

    mptest 0.0.1 (new formula)

--- Formula/acosidlinux.rb
+++ Formula/acosidlinux.rb
@@ -5,6 +5,10 @@ class Acosidlinux < Formula
   version "0.0.3"
   sha256 "1a7fe174fed18ac60cea05702630c44f5a266145f62e1e7ab9c8b0cb41db4388"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6d56e0e033752d15530b4f7a2fa41ad6cc5e9f63bf3706e05c1e0b9c6a732ca3"
+  end
+
   on_linux do
     def install
       prefix.install Dir["*"]
