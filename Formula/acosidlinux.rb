c29aade023a8f2c3fac85c65f06cc5aa60c0d17f
test <markus.punz@bdc.at>
2023-06-15 17:04:50 +0200

    test

--- Formula/acosidlinux.rb
+++ Formula/acosidlinux.rb
@@ -6,7 +6,8 @@ class Acosidlinux < Formula
   sha256 "1a7fe174fed18ac60cea05702630c44f5a266145f62e1e7ab9c8b0cb41db4388"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6d56e0e033752d15530b4f7a2fa41ad6cc5e9f63bf3706e05c1e0b9c6a732ca3"
+    root_url "https://github.com/MarkusPunz/homebrew-tapacosidlinux/releases/download/acosidlinux-0.0.3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "35faa0bdde9eb92ff88da06fc5237a81df2ea3abb918aac9486bade954c73539"
   end
 
   on_linux do
