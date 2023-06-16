b5834878110708216506996a3363dad60546fb1a
test <markus.punz@bdc.at>
2023-06-16 08:16:55 +0200

    (new formula)

--- Formula/acosidlinux.rb
+++ Formula/acosidlinux.rb
@@ -2,17 +2,18 @@ class Acosidlinux < Formula
   desc "Test MPTest"
   homepage "https://www.bdc.at"
   url "https://github.com/MarkusPunz/testapp1/raw/main/linux.tar.gz"
-  version "0.0.3"
-  sha256 "1a7fe174fed18ac60cea05702630c44f5a266145f62e1e7ab9c8b0cb41db4388"
+  version "0.0.4"
+  sha256 "645b0f5277c846febd5a71e6207bf8a677a87ccb69f29eec64237d855a6ac4f8"
 
   bottle do
-    root_url "https://github.com/MarkusPunz/homebrew-tapacosidlinux/releases/download/acosidlinux-0.0.3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "35faa0bdde9eb92ff88da06fc5237a81df2ea3abb918aac9486bade954c73539"
+    root_url "https://github.com/MarkusPunz/homebrew-tapacosidlinux/releases/download/acosidlinux-0.0.4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2d291790b9a772948d337c1536efcd318ebee5bb2167d8608d8979651162805c"
   end
 
   on_linux do
     def install
       prefix.install Dir["*"]
+      lib.install_symlink prefix/"libacosid-pkcs11.so"
     end
   end
 
